0c23c675410f8a5a854ad153c5fe86bedd447ce0
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-06-20 18:13:29 +0300

    created comment controler

--- router.js
+++ router.js
@@ -3,8 +3,12 @@ const router = Router();
 
 const authControler = require("./routeControler/authControler");
 const hacksControler = require("./routeControler/hacksControler");
+const commentControler = require("./routeControler/commentControler");
+
+const isAuth = require("./middlewares/isAuth");
 
 router.use("/auth", authControler);
-router.use("/hack", hacksControler)
+router.use("/hack", hacksControler);
+router.use("/comment", isAuth, commentControler);
 
 module.exports = router;
---
+++ services/commentService.js
@@ -0,0 +1,9 @@
+const Commnet = require("../schemes/Comment");
+
+async function create(data){
+
+}
+
+module.exports = {
+    create,
+}
